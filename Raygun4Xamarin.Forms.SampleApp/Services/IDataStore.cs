a77a1bc3c701d5cb10f10b86300cd5de3c3affbc
Mitchell Duncan <mitchell@raygun.com>
2019-06-25 16:44:05 +1200

    Move the test calls into app items

--- Raygun4Xamarin.Forms.SampleApp/Services/IDataStore.cs
+++ Raygun4Xamarin.Forms.SampleApp/Services/IDataStore.cs
@@ -10,6 +10,6 @@ namespace Raygun.Forms.SampleApp.Services
     Task<bool> UpdateItemAsync(T item);
     Task<bool> DeleteItemAsync(string id);
     Task<T> GetItemAsync(string id);
-    Task<IEnumerable<T>> GetItemsAsync();
+    Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
   }
 }
